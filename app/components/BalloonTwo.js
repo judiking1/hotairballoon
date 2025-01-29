f56ba93cfc99c50aa60b01e63301c7d433a4820f
LeeWonbae <judiking1@gmail.com>
2025-01-29 17:07:08 +0900

    version 0.4.0, 풍선 회전 추가, 새 풍선 하나 추가

--- app/components/BalloonTwo.js
+++ app/components/BalloonTwo.js
@@ -1,15 +1,15 @@
+import React, { useRef, useState } from 'react';
 import { useGLTF } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
-import { useRef, useState } from 'react';
 
-export function Balloon({
+export function BalloonTwo({
   radius = 100,
   height = 150,
   speed = 1,
   initialAngle = 0,
 }) {
   const groupRef = useRef();
-  const { nodes, materials } = useGLTF('/model/balloon/balloon0.glb');
+  const { nodes, materials } = useGLTF('/model/balloon/balloon2.glb');
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
   useFrame((state, delta) => {
@@ -17,27 +17,25 @@ export function Balloon({
     const t = state.clock.getElapsedTime();
     const angle = t * speed + initialAngle;
     groupRef.current.position.x = Math.cos(angle) * radius;
-    groupRef.current.position.y = height - 50+20*Math.cos(angle);
+    groupRef.current.position.y = height - 50 + 20 * Math.cos(angle);
     groupRef.current.position.z = Math.sin(angle) * radius;
     groupRef.current.rotation.y = Math.sin(t) * 0.8;
   });
-
   return (
     <group
       dispose={null}
       ref={groupRef}
-      scale={hovered ? 55 : 50}
+      scale={hovered ? 1.1 : 1}
       onClick={() => setActive(!active)}
       onPointerOver={() => setHover(true)}
       onPointerOut={() => setHover(false)}
     >
       <mesh
-        geometry={nodes.model.geometry}
-        material={materials.CustomMaterial}
-        rotation={[Math.PI / 2, 0, 0]}
+        geometry={nodes.Hot_Air_Balloon.geometry}
+        material={materials.Mat}
       />
     </group>
   );
 }
 
-useGLTF.preload('/model/balloon/balloon0.glb');
+useGLTF.preload('/model/balloon/balloon2.glb');
--- app/page.js
+++ app/page.js
@@ -3,12 +3,13 @@
 import { Canvas } from '@react-three/fiber';
 import { Cloud, Clouds, Float, OrbitControls, Sky } from '@react-three/drei';
 import Castle from './components/Castle';
-import { random } from 'maath'
-import { MeshLambertMaterial } from 'three';
-import { Balloon } from './components/balloon';
+import { random } from 'maath';
+import { MeshLambertMaterial, NoToneMapping } from 'three';
+import { Balloon } from './components/Balloon';
+import { BalloonTwo } from './components/BalloonTwo';
 
-const box = random.inBox(new Float32Array(100 * 3), { sides: [1, 1, 1] })
-const spherical = random.onSphere(box, { radius: 1 })
+const box = random.inBox(new Float32Array(20 * 3), { sides: [4, 1, 4] });
+const spherical = random.onSphere(box, { radius: 1 });
 
 export default function IndexPage() {
   return (
@@ -19,17 +20,20 @@ export default function IndexPage() {
         powerPreference: 'high-performance',
         alpha: false,
         antialias: true,
+        toneMapping: NoToneMapping,
       }}
-      camera={{ position: [-70, 90, 100], fov: 100}}
+      camera={{ position: [-70, 90, 100], fov: 75 }}
     >
       <OrbitControls
         target={[0, 60, 0]}
-        minDistance={50}
+        minDistance={10}
         maxDistance={200}
+        minPolarAngle={-Math.PI / 2}
+        maxPolarAngle={Math.PI / 2}
       />
       <Sky
         distance={500000}
-        sunPosition={[0, 0, -1]} // 태양의 위치
+        sunPosition={[0, 0, -100]} // 태양의 위치
         inclination={0.4} // 하늘 빛 기울기
         turbidity={10} // 하늘의 탁도 (값이 높을수록 더 탁해짐)
         rayleigh={2} // 레일리 산란 (값이 높을수록 하늘이 더 파랗게 보임)
@@ -41,25 +45,18 @@ export default function IndexPage() {
           <Cloud
             segments={spherical.length}
             seed={1}
-            volume={100}
-            growth={200}
+            volume={50}
+            growth={50}
             smallestVolume={0.5}
-            bounds={300}
-            opacity={0.4}
+            bounds={180}
+            opacity={0.3}
             speed={0.1}
             concentrate="random"
-            distribute={(cloud, index) => ({
-              point: {
-                x: spherical[index * 3 + 0],
-                y: spherical[index * 3 + 1],
-                z: spherical[index * 3 + 2],
-              },
-              //volume: Math.random(), // Optional
-            })}
           />
         </Float>
       </Clouds>
-      <Balloon position={[0, 155, -500]} />
+      <Balloon radius={80} speed={0.2} height={150} initialAngle={0} />
+      <BalloonTwo radius={80} speed={0.2} height={150} initialAngle={3.14} />
       <ambientLight intensity={5.5} />
       <Castle position={[0, -50, 0]} scale={[5, 5, 5]} />
     </Canvas>
